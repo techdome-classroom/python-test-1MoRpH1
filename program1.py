876638cfff0727bbbabc63c0a8693c541a619367
1MoRpH1 <sharmagreat2002@gmail.com>
2024-05-09 12:41:23 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,12 +1,12 @@
 class Solution:
-    def explore_island(self, grid, row, col, visited):
+    def scan_island(self, grid, row, col, visited):
         if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == 'W' or visited[row][col]:
             return
         visited[row][col] = True
-        self.explore_island(grid, row + 1, col, visited)
-        self.explore_island(grid, row - 1, col, visited)
-        self.explore_island(grid, row, col + 1, visited)
-        self.explore_island(grid, row, col - 1, visited)
+        self.scan_island(grid, row + 1, col, visited)
+        self.scan_island(grid, row - 1, col, visited)
+        self.scan_island(grid, row, col + 1, visited)
+        self.scan_island(grid, row, col - 1, visited)
 
     def getTotalIsles(self, grid: list[list[str]]) -> int:
         if not grid:
@@ -17,6 +17,6 @@ class Solution:
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 'L' and not visited[i][j]:
-                    self.explore_island(grid, i, j, visited)
+                    self.scan_island(grid, i, j, visited)
                     island_count += 1
         return island_count
