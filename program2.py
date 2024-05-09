4422f1fddad4f5c697748016fb0de0b18852143f
1MoRpH1 <sharmagreat2002@gmail.com>
2024-05-09 13:03:10 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,13 +1,13 @@
 def decode_message(message: str, decoder_key: str) -> bool:
-    if len(message) != len(decoder_key):  # Check if lengths are equal
+    if len(message) != len(decoder_key):  
         return False
     
-    for i in range(len(message)):  # Iterate through each character
-        if decoder_key[i] == '?':  # Check if it's a question mark
-            continue  # Move to the next character
-        elif decoder_key[i] == '*':  # Check if it's a star symbol
-            continue  # Move to the next character
-        elif decoder_key[i] != message[i]:  # Check if characters don't match
-            return False  # If they don't match, return False
+    for i in range(len(message)):  
+        if decoder_key[i] == '?':  
+            continue  
+        elif decoder_key[i] == '*': 
+            continue  
+        elif decoder_key[i] != message[i]:  
+            return False  
     
-    return True  # If all characters match or are wildcard symbols, return True
+    return True  
